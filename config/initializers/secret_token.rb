bfd4bcec9cd61bf11b85e37f089f9a75d5186edb
tokada <ubiqnet@gmail.com>
2013-05-27 21:30:55 +0900

    rails new picsy-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PicsyRails::Application.config.secret_key_base = '1b0c0665a827bbc7316a881e8e060e103c6b8d05a0bf4a6abd2fad2d5f7b83069ba504712e25c35c335912bbd09f9e64449ef6cb10d392a77001bd48807c7c0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PicsyRails::Application.config.session_store :cookie_store, key: '_picsy-rails_session'
