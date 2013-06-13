a8dcc401e0618e61d8805ebd476de597db401842
tokada <ubiqnet@gmail.com>
2013-06-13 12:20:38 +0900

    precompile all assets

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,22 @@ module PicsyRails
     Devise.setup do |config|
       config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'], :display => 'popup'
     end
+
+    # prepile all assets
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
   end
 end
